dbb97467f68ff76766b6d767c9f06185f14787c6
Om Prakash Behera <bomprakash485@gmail.com>
2026-09-14 17:13:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int findEquilibrium(int arr[]) {
+        // code here
+        int n = arr.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        
+        prefix[0] = arr[0];
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        
+        suffix[n - 1] = arr[n - 1];
+        for(int i = n - 2; i>= 0; i--){
+            suffix[i] = suffix[i + 1] + arr[i];
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(prefix[i] == suffix[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
